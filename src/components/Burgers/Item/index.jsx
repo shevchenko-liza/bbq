cd94dca3b7c7430fa2d016262f31c2cf86f865af
Vadim, Tsimbal <devoliisher@gmail.com>
2022-08-05 20:36:04 +0300

    Update: Burger layouting.

--- src/components/Burgers/Item/index.jsx
+++ src/components/Burgers/Item/index.jsx
@@ -1,32 +1,43 @@
+import { useCallback, useState } from 'react'
+import clsx from 'clsx'
 
 import classes from '../styles.module.scss'
 
+export const Order = () => {
+  const [amount, setAmount] = useState(1)
+  const handleChange = useCallback(event => {
+    setAmount(event.target.value)
+  }, [])
 
-export const Button=({button,form})=>(
-    (
-        <div>
-        <button className={classes.button}>{button}Заказать</button>
-        <form className={classes.form}>{form}
-        <button type="reset" value="-">-</button>
-        <input type="text" size="1"></input>
-        <button type="submit" value="+">+</button>
-  
-        </form>
-        
-        </div>
-    )
-)
+  const handleSubmit = useCallback(event => {
+    event.preventDefault()
 
-export const Item = ({name,compound,weight,photo,button}) => (
+    console.log({ id: NaN, amount })
+  }, [amount])
+
+  return (
+    <form className={classes.form} onSubmit={handleSubmit}>
+      <div className={classes.inputBox}>
+        <button type="button" className={clsx(classes.button, classes.decrease)} value="-">-</button>
+        <input type="text" size="3" className={classes.input} value={amount} onChange={handleChange} />
+        <button type="button" className={clsx(classes.button, classes.increase)} value="+">+</button>
+      </div>
+      <button type="submit" className={clsx(classes.button, classes.order)}>Заказать</button>
+    </form>
+  )
+}
+
+export const Item = ({ name, compound, weight, photo }) => (
   <div className={classes.item}>
-    <span className={classes.name}>{name}</span>
+    <div className={classes.photoBox}>
       <img className={classes.photo} src={photo} alt="" />
-    
-    <span className={classes.compound}>{compound}</span>
-    <span className={classes.weight}>{weight}</span>
-
-    <Button/>
-    
+    </div>
+    <div className={classes.infoBox}>
+      <h3 className={classes.name}>{name}</h3>
+      <p className={classes.compound}>Состав: {compound.join(', ')}</p>
+      <span className={classes.weight}>{weight}</span>
+      <Order />
+    </div>
   </div>
 )
 
--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,11 @@ import { BrowserRouter } from "react-router-dom"
 
 import App from './App'
 
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 import './index.scss'
 
 const MOUNT_POINT = document.getElementById('root')
